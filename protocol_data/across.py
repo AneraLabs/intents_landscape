e73f1d37336ee05a8b4272118c831bbf76d80923
Konrad <konrad.strachan@gmail.com>
2024-02-26 12:31:55 +0000

    Added supported chains

--- protocol_data/across.py
+++ protocol_data/across.py
@@ -4,6 +4,9 @@ def get_contract_address(chain_id):
     # Optionally configurable to be different per chain_id
     return "0x1234"
 
+def get_supported_chains():
+    return [1, 10]
+
 def get_deposit_function_filter():
     return "" # or None if no filter to use
 
