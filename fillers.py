5c79b0fb4739f06592e31d0bbc86deca07e99115
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2024-06-26 17:56:47 +0400

    add filler getter by address

--- fillers.py
+++ fillers.py
@@ -21,3 +21,10 @@ for file in os.listdir('intents_landscape/fillers'):
     with open(f'intents_landscape/fillers/{file}', 'r') as f:
         filler_dict = json.load(f)
         KNOWN_FILLERS.append(Filler.from_dict(filler_dict))
+
+
+def get_filler_by_address(address: str) -> Filler | None:
+    for filler in KNOWN_FILLERS:
+        if address in filler.addresses:
+            return filler
+    return None
