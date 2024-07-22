ad6fff8fab37f430b3dd30e338bf6d3ba6f4832b
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2024-07-22 19:39:06 +0400

    add info about native token to chains

--- chains/chain_info.py
+++ chains/chain_info.py
@@ -1,31 +1,47 @@
 from dataclasses import dataclass
 
 
-@dataclass
+@dataclass(frozen=True)
 class Chain:
     _id: str
     name: str
     icon_url: str
     average_block_time: float | None
+    native_token_symbol: str = 'ETH'
+    native_token_icon_url: str = 'https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg'
+    native_token_decimals: int = 18
 
 CHAINS = {
     '1': Chain('1', 'Ethereum', 'https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg', 12.0),
     '10': Chain('10', 'Optimism', 'https://icons.llamao.fi/icons/chains/rsz_optimism.jpg', 2.0),
-    '137': Chain('137', 'Polygon', 'https://icons.llamao.fi/icons/chains/rsz_polygon.jpg', 2.0),
+    '137': Chain('137', 'Polygon', 'https://icons.llamao.fi/icons/chains/rsz_polygon.jpg', 2.0, 
+                 native_token_symbol='MATIC', 
+                 native_token_icon_url='https://icons.llamao.fi/icons/chains/rsz_polygon.jpg'),
     '42161': Chain('42161', 'Arbitrum', 'https://icons.llamao.fi/icons/chains/rsz_arbitrum.jpg', 0.26),
     '8453': Chain('8453', 'Base', 'https://icons.llamao.fi/icons/chains/rsz_base.jpg', 2.0),
     '324': Chain('324', 'zkSync Era', 'https://icons.llamao.fi/icons/chains/rsz_zksync-era.jpg', 1.1),
-    '7565164': Chain('7565164', 'Solana', 'https://icons.llamao.fi/icons/chains/rsz_solana.jpg', 0.46),
+    '7565164': Chain('7565164', 'Solana', 'https://icons.llamao.fi/icons/chains/rsz_solana.jpg', 0.46, 
+                     native_token_symbol='SOL', 
+                     native_token_icon_url='https://icons.llamao.fi/icons/chains/rsz_solana.jpg',
+                     native_token_decimals=9),
     '43114': Chain('43114', 'Avalanche', 'https://icons.llamao.fi/icons/chains/rsz_avalanche.jpg', 2.1),
-    '56': Chain('56', 'BNB Chain', 'https://icons.llamao.fi/icons/chains/rsz_binance.jpg', 3.1),
+    '56': Chain('56', 'BNB Chain', 'https://icons.llamao.fi/icons/chains/rsz_binance.jpg', 3.1, 
+                native_token_symbol='BNB', 
+                native_token_icon_url='https://icons.llamao.fi/icons/chains/rsz_binance.jpg'),
     '59144': Chain('59144', 'Linea', 'https://icons.llamao.fi/icons/chains/rsz_linea.jpg', 2.0),
     '81457': Chain('81457', 'Blast', 'https://icons.llamao.fi/icons/chains/rsz_blast.jpg', 2.0),
     '534352': Chain('534352', 'Scroll', 'https://icons.llamao.fi/icons/chains/rsz_scroll.jpg', 2.96),
     '1101': Chain('1101', 'Polygon zkEVM', 'https://icons.llamao.fi/icons/chains/rsz_polygon-zkevm.jpg', 3.0),
     '5000': Chain('5000', 'Mantle', 'https://icons.llamao.fi/icons/chains/rsz_mantle.jpg', None),
     '169': Chain('169', 'Manta', 'https://icons.llamao.fi/icons/chains/rsz_manta.jpg', 10.0),
-    '30': Chain('30', 'Rootstock', 'https://icons.llamao.fi/icons/chains/rsz_rootstock.jpg', None),
-    '728126428': Chain('728126428', 'Tron', 'https://icons.llamao.fi/icons/chains/rsz_tron.jpg', 3.0),
+    # '30': Chain('30', 'Rootstock', 'https://icons.llamao.fi/icons/chains/rsz_rootstock.jpg', None, 
+    #             # TODO: verify RBTC has 18 decimals
+    #             native_token_symbol='RBTC', 
+    #             native_token_icon_url='https://icons.llamao.fi/icons/chains/rsz_rootstock.jpg'),
+    '728126428': Chain('728126428', 'Tron', 'https://icons.llamao.fi/icons/chains/rsz_tron.jpg', 3.0, 
+                       native_token_symbol='TRX', 
+                       native_token_icon_url='https://icons.llamao.fi/icons/chains/rsz_tron.jpg',
+                       native_token_decimals=6),
     '34443': Chain('34443', 'Mode', 'https://icons.llamao.fi/icons/chains/rsz_mode.jpg', None),
     '288': Chain('288', 'Boba Network', 'https://icons.llamao.fi/icons/chains/rsz_boba.jpg', None),
     '1088': Chain('1088', 'Metis', 'https://icons.llamao.fi/icons/chains/rsz_metis.jpg', None),
