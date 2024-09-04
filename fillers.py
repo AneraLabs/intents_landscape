b842cf4a709a50bd8a1da40799409b372a1ce524
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2024-09-04 16:29:45 +0400

    fix fillers json loading

--- fillers.py
+++ fillers.py
@@ -18,8 +18,9 @@ KNOWN_FILLERS: list[Filler] = []
 
 # all files in the fillers folder
 for file in os.listdir('intents_landscape/fillers'):
-    with open(f'intents_landscape/fillers/{file}', 'r') as f:
-        filler_dict = json.load(f)
+    if file.endswith('.json'):
+        with open(f'intents_landscape/fillers/{file}', 'r') as f:
+            filler_dict = json.load(f)
         KNOWN_FILLERS.append(Filler.from_dict(filler_dict))
 
 
@@ -35,3 +36,9 @@ def get_filler_by_name(name: str) -> Filler | None:
         if filler.name == name:
             return filler
     return None
+
+
+if __name__ == '__main__':
+    # check if all fillers are loaded with no errors
+    for filler in KNOWN_FILLERS:
+        print(filler.name)
